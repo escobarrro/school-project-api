0799c96b73767eccbc72a96efe5b93604a504d92
escobarrro <pblplrz@gmail.com>
2023-04-17 22:59:24 +0200

    fix

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'pepcio321',
-    database : 'react-project'
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
   }
 });
 
